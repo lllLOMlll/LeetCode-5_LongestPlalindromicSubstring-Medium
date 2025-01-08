7c945694a4938343ce7d595d8ae7e9a467bcd0bd
Louis-Olivier Major <louis.olivier.major@gmail.com>
2025-01-07 19:30:54 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public string LongestPalindrome(string s) {
+        return "laval";
+    }
+
+static void Main(string[] args)
+{
+    Solution s = new Solution();
+
+    string input1 = "babad";
+    Console.WriteLine("bab = " + s.LongestPalindrome(input1));
+
+     string input2 = "cbbd";
+    Console.WriteLine("bb = " + s.LongestPalindrome(input2));
+}
+}
